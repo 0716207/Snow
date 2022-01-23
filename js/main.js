76a624d698e0e86db657b72777c939ca5a5ac13e
xm3karen <55311079+karenluuuu3@users.noreply.github.com>
2022-01-24 02:09:35 +0800

    220124

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
