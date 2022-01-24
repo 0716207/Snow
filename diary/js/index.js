17e79c8b7607e56e5ca0353e5b3eea85677934b4
xm3karen <55311079+karenluuuu3@users.noreply.github.com>
2022-01-25 00:03:47 +0800

    220124

---
+++ diary/js/index.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+// switch pages : first level
+$(document).ready(function(){	
+	$(".teru_box").eq(0).addClass("teru_ch_bg");
+	$(".teru_content").eq(0).show();
+
+	$(".teru_box").click(function(){
+		var _index = $(this).index();
+		$(this).addClass("teru_ch_bg").siblings().removeClass("teru_ch_bg");
+		$(".teru_content").eq(_index).fadeIn(1500).siblings().fadeOut(500);		
+	});	
+});
+
+
+// fade to another page
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
