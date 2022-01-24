e96c57fd35c927b9633fa4f4377cfca3c71d280b
xm3karen <55311079+karenluuuu3@users.noreply.github.com>
2022-01-24 22:22:00 +0800

    220124

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 // JavaScript Document
 
+// switch pages : first level
+$(document).ready(function(){	
+	$(".box").eq(0).addClass("ch_bg");
+	$(".content").eq(0).show();
+
+	$(".box").click(function(){
+		var _index = $(this).index();
+		$(this).addClass("ch_bg").siblings().removeClass("ch_bg");
+		$(".content").eq(_index).fadeIn(1500).siblings().fadeOut(500);		
+	});	
+});
+
+
+// fade to another page
 window.transitionToPage = function(href) {
     document.querySelector('body').style.opacity = 0
     setTimeout(function() { 
