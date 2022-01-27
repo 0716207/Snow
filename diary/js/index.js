243c6ef2f82261adaa9a55e16970bb2c3d37f163
xm3karen <55311079+karenluuuu3@users.noreply.github.com>
2022-01-28 01:14:49 +0800

    220128
    teru finish

--- diary/js/index.js
+++ diary/js/index.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
-// switch pages : first level
 $(document).ready(function(){	
+	// switch pages
 	$(".teru_box").eq(0).addClass("teru_ch_bg");
 	$(".content").eq(0).show();
 
@@ -10,6 +10,15 @@ $(document).ready(function(){
 		$(this).addClass("teru_ch_bg").siblings().removeClass("teru_ch_bg");
 		$(".content").eq(_index).fadeIn(1500).siblings().fadeOut(500);		
 	});	
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.totop').fadeIn();
+		} else {
+			$('.totop').fadeOut();
+		}
+	});
 });
 
 
@@ -24,3 +33,31 @@ window.transitionToPage = function(href) {
 document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('body').style.opacity = 1
 })
+
+/*totop*/
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 // JavaScript Document
 
-// switch pages : first level
+
 $(document).ready(function(){	
+	// switch pages
 	$(".box").eq(0).addClass("ch_bg");
 	$(".content").eq(0).show();
 
@@ -10,6 +11,16 @@ $(document).ready(function(){
 		$(this).addClass("ch_bg").siblings().removeClass("ch_bg");
 		$(".content").eq(_index).fadeIn(1500).siblings().fadeOut(500);		
 	});	
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 300) {
+			$('.totop').fadeIn();
+		} else {
+			$('.totop').fadeOut();
+		}
+	});
+
 });
 
 
@@ -24,3 +35,30 @@ window.transitionToPage = function(href) {
 document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('body').style.opacity = 1
 })
+
+/*totop*/
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
